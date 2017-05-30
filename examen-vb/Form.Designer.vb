ee5ef3b34ce491ee3d5cc11a85346c9b874c3991
Pablo González <pagoru@gmail.com>
2017-05-30 15:20:46 +0100

    All

---
+++ examen-vb/Form.Designer.vb
@@ -0,0 +1,338 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PreuAmbIVA = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Comentaris = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.EstocActual = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.CodiFamilia = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Preu = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DescripcioArticle = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CodiArticle = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Modificar = New System.Windows.Forms.Button()
+        Me.Eliminar = New System.Windows.Forms.Button()
+        Me.Crear = New System.Windows.Forms.Button()
+        Me.ModificarEstoc = New System.Windows.Forms.Button()
+        Me.Primer = New System.Windows.Forms.Button()
+        Me.Anterior = New System.Windows.Forms.Button()
+        Me.Ultim = New System.Windows.Forms.Button()
+        Me.Seguent = New System.Windows.Forms.Button()
+        Me.Acceptar = New System.Windows.Forms.Button()
+        Me.Cancelar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.PreuAmbIVA)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Comentaris)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.EstocActual)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.CodiFamilia)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Preu)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.DescripcioArticle)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.CodiArticle)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(219, 208)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Article"
+        '
+        'PreuAmbIVA
+        '
+        Me.PreuAmbIVA.Location = New System.Drawing.Point(113, 177)
+        Me.PreuAmbIVA.Name = "PreuAmbIVA"
+        Me.PreuAmbIVA.ReadOnly = True
+        Me.PreuAmbIVA.Size = New System.Drawing.Size(97, 20)
+        Me.PreuAmbIVA.TabIndex = 13
+        '
+        'Label7
+        '
+        Me.Label7.Location = New System.Drawing.Point(7, 176)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(100, 20)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "PreuAmbIVA"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Comentaris
+        '
+        Me.Comentaris.Location = New System.Drawing.Point(113, 151)
+        Me.Comentaris.Name = "Comentaris"
+        Me.Comentaris.ReadOnly = True
+        Me.Comentaris.Size = New System.Drawing.Size(97, 20)
+        Me.Comentaris.TabIndex = 11
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(7, 150)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(100, 20)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Comentaris"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'EstocActual
+        '
+        Me.EstocActual.Location = New System.Drawing.Point(113, 125)
+        Me.EstocActual.Name = "EstocActual"
+        Me.EstocActual.ReadOnly = True
+        Me.EstocActual.Size = New System.Drawing.Size(97, 20)
+        Me.EstocActual.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(7, 124)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "EstocActual"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'CodiFamilia
+        '
+        Me.CodiFamilia.Location = New System.Drawing.Point(113, 99)
+        Me.CodiFamilia.Name = "CodiFamilia"
+        Me.CodiFamilia.ReadOnly = True
+        Me.CodiFamilia.Size = New System.Drawing.Size(97, 20)
+        Me.CodiFamilia.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(7, 98)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(100, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "CodiFamilia"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Preu
+        '
+        Me.Preu.Location = New System.Drawing.Point(113, 73)
+        Me.Preu.Name = "Preu"
+        Me.Preu.ReadOnly = True
+        Me.Preu.Size = New System.Drawing.Size(97, 20)
+        Me.Preu.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(7, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Preu"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'DescripcioArticle
+        '
+        Me.DescripcioArticle.Location = New System.Drawing.Point(113, 47)
+        Me.DescripcioArticle.Name = "DescripcioArticle"
+        Me.DescripcioArticle.ReadOnly = True
+        Me.DescripcioArticle.Size = New System.Drawing.Size(97, 20)
+        Me.DescripcioArticle.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(7, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "DescripcioArticle"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'CodiArticle
+        '
+        Me.CodiArticle.Location = New System.Drawing.Point(113, 21)
+        Me.CodiArticle.Name = "CodiArticle"
+        Me.CodiArticle.ReadOnly = True
+        Me.CodiArticle.Size = New System.Drawing.Size(97, 20)
+        Me.CodiArticle.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CodiArticle"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Modificar
+        '
+        Me.Modificar.Location = New System.Drawing.Point(238, 59)
+        Me.Modificar.Name = "Modificar"
+        Me.Modificar.Size = New System.Drawing.Size(97, 20)
+        Me.Modificar.TabIndex = 15
+        Me.Modificar.Text = "Modificar"
+        Me.Modificar.UseVisualStyleBackColor = True
+        '
+        'Eliminar
+        '
+        Me.Eliminar.Location = New System.Drawing.Point(238, 86)
+        Me.Eliminar.Name = "Eliminar"
+        Me.Eliminar.Size = New System.Drawing.Size(97, 20)
+        Me.Eliminar.TabIndex = 16
+        Me.Eliminar.Text = "Eliminar"
+        Me.Eliminar.UseVisualStyleBackColor = True
+        '
+        'Crear
+        '
+        Me.Crear.Location = New System.Drawing.Point(238, 34)
+        Me.Crear.Name = "Crear"
+        Me.Crear.Size = New System.Drawing.Size(97, 20)
+        Me.Crear.TabIndex = 17
+        Me.Crear.Text = "Crear"
+        Me.Crear.UseVisualStyleBackColor = True
+        '
+        'ModificarEstoc
+        '
+        Me.ModificarEstoc.Location = New System.Drawing.Point(238, 138)
+        Me.ModificarEstoc.Name = "ModificarEstoc"
+        Me.ModificarEstoc.Size = New System.Drawing.Size(97, 20)
+        Me.ModificarEstoc.TabIndex = 18
+        Me.ModificarEstoc.Text = "Modificar Estoc"
+        Me.ModificarEstoc.UseVisualStyleBackColor = True
+        Me.ModificarEstoc.Visible = False
+        '
+        'Primer
+        '
+        Me.Primer.Location = New System.Drawing.Point(23, 227)
+        Me.Primer.Name = "Primer"
+        Me.Primer.Size = New System.Drawing.Size(61, 20)
+        Me.Primer.TabIndex = 18
+        Me.Primer.Text = "<<"
+        Me.Primer.UseVisualStyleBackColor = True
+        '
+        'Anterior
+        '
+        Me.Anterior.Location = New System.Drawing.Point(90, 227)
+        Me.Anterior.Name = "Anterior"
+        Me.Anterior.Size = New System.Drawing.Size(61, 20)
+        Me.Anterior.TabIndex = 19
+        Me.Anterior.Text = "<"
+        Me.Anterior.UseVisualStyleBackColor = True
+        '
+        'Ultim
+        '
+        Me.Ultim.Location = New System.Drawing.Point(229, 227)
+        Me.Ultim.Name = "Ultim"
+        Me.Ultim.Size = New System.Drawing.Size(61, 20)
+        Me.Ultim.TabIndex = 20
+        Me.Ultim.Text = ">>"
+        Me.Ultim.UseVisualStyleBackColor = True
+        '
+        'Seguent
+        '
+        Me.Seguent.Location = New System.Drawing.Point(162, 227)
+        Me.Seguent.Name = "Seguent"
+        Me.Seguent.Size = New System.Drawing.Size(61, 20)
+        Me.Seguent.TabIndex = 21
+        Me.Seguent.Text = ">"
+        Me.Seguent.UseVisualStyleBackColor = True
+        '
+        'Acceptar
+        '
+        Me.Acceptar.Location = New System.Drawing.Point(238, 190)
+        Me.Acceptar.Name = "Acceptar"
+        Me.Acceptar.Size = New System.Drawing.Size(97, 20)
+        Me.Acceptar.TabIndex = 22
+        Me.Acceptar.Text = "Acceptar"
+        Me.Acceptar.UseVisualStyleBackColor = True
+        Me.Acceptar.Visible = False
+        '
+        'Cancelar
+        '
+        Me.Cancelar.Location = New System.Drawing.Point(238, 164)
+        Me.Cancelar.Name = "Cancelar"
+        Me.Cancelar.Size = New System.Drawing.Size(97, 20)
+        Me.Cancelar.TabIndex = 23
+        Me.Cancelar.Text = "Cancel·lar"
+        Me.Cancelar.UseVisualStyleBackColor = True
+        Me.Cancelar.Visible = False
+        '
+        'Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(341, 261)
+        Me.Controls.Add(Me.ModificarEstoc)
+        Me.Controls.Add(Me.Cancelar)
+        Me.Controls.Add(Me.Acceptar)
+        Me.Controls.Add(Me.Seguent)
+        Me.Controls.Add(Me.Ultim)
+        Me.Controls.Add(Me.Anterior)
+        Me.Controls.Add(Me.Primer)
+        Me.Controls.Add(Me.Crear)
+        Me.Controls.Add(Me.Eliminar)
+        Me.Controls.Add(Me.Modificar)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form"
+        Me.ShowIcon = False
+        Me.Text = "Examen - Pablo González Rubio"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PreuAmbIVA As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Comentaris As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents EstocActual As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents CodiFamilia As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Preu As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DescripcioArticle As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CodiArticle As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Modificar As Button
+    Friend WithEvents Eliminar As Button
+    Friend WithEvents Crear As Button
+    Friend WithEvents ModificarEstoc As Button
+    Friend WithEvents Primer As Button
+    Friend WithEvents Anterior As Button
+    Friend WithEvents Ultim As Button
+    Friend WithEvents Seguent As Button
+    Friend WithEvents Acceptar As Button
+    Friend WithEvents Cancelar As Button
+End Class
---
+++ examen-vb/Form.vb
@@ -0,0 +1,234 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form
+
+    Private sqlConnection As SqlConnection
+
+    Private ds_articles, ds_families As DataSet
+    Private sda_articles, sda_families As SqlDataAdapter
+    Private bmb_articles As BindingManagerBase
+
+    Private columns_article As String() = {"CODIARTICLE", "DESCRIPCIOARTICLE", "PREU", "CODIFAMILIA", "ESTOCACTUAL", "COMENTARIS", "PREUAMBIVA"}
+
+    Private PROXNUMARTICLE As Integer
+
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        sqlConnection = New SqlConnection()
+        sqlConnection.ConnectionString = "Data Source=.\SQLEXPRESS;Initial Catalog=EX02; Trusted_Connection=True;"
+        sqlConnection.Open()
+
+
+        sda_articles = New SqlDataAdapter("SELECT * FROM ARTICLES ORDER BY codiarticle", sqlConnection)
+        Dim sqlCommandBuilder As SqlCommandBuilder = New SqlCommandBuilder(sda_articles)
+
+        ds_articles = New DataSet()
+        sda_articles.Fill(ds_articles, "Articles")
+        BindArticles()
+
+        sda_families = New SqlDataAdapter("SELECT * FROM FAMILIES", sqlConnection)
+        sqlCommandBuilder = New SqlCommandBuilder(sda_families)
+
+        ds_families = New DataSet()
+        sda_families.Fill(ds_families, "Families")
+
+
+        SelectorsUpdate()
+    End Sub
+
+    Private Function GetProxNumArticle()
+        Dim int As Integer
+        Dim read As SqlDataReader
+        Dim sqlCommand = New SqlCommand
+        sqlCommand.Connection = sqlConnection
+        sqlCommand.CommandText = "SELECT PROXNUMARTICLE FROM NUMERACIONS"
+
+        read = sqlCommand.ExecuteReader()
+        read.Read()
+        int = Integer.Parse(read(0))
+        read.Close()
+
+        Return int
+    End Function
+
+    Private Sub IncrementProxNumArticle()
+        Dim sqlCommand = New SqlCommand
+        sqlCommand.Connection = sqlConnection
+        sqlCommand.CommandText = "UPDATE NUMERACIONS SET PROXNUMARTICLE += 1"
+
+        sqlCommand.ExecuteNonQuery()
+    End Sub
+
+    Private Sub BindArticles()
+        Dim inputs_article As TextBox() = {CodiArticle, DescripcioArticle, Preu, CodiFamilia, EstocActual, Comentaris, PreuAmbIVA}
+
+        For index As Integer = 0 To columns_article.Length - 1 Step 1
+            inputs_article(index).DataBindings.Add(New Binding("Text", ds_articles, "Articles." + columns_article(index)))
+        Next
+        bmb_articles = BindingContext(ds_articles, "Articles")
+    End Sub
+
+    Private Sub LeftSelectorsEnabled(bool As Boolean)
+        Primer.Enabled = bool
+        Anterior.Enabled = bool
+    End Sub
+
+    Private Sub RightSelectorsenabled(bool As Boolean)
+        Ultim.Enabled = bool
+        Seguent.Enabled = bool
+    End Sub
+
+    Private Sub SelectorsUpdate()
+        LeftSelectorsEnabled(True)
+        RightSelectorsenabled(True)
+        If bmb_articles.Count > 1 Then
+            If bmb_articles.Position = 0 Then
+                LeftSelectorsEnabled(False)
+            ElseIf bmb_articles.Count - 1 = bmb_articles.Position Then
+                RightSelectorsenabled(False)
+            End If
+            Return
+        End If
+        LeftSelectorsEnabled(False)
+        RightSelectorsenabled(False)
+    End Sub
+
+    Private Sub Primer_Click(sender As Object, e As EventArgs) Handles Primer.Click
+        bmb_articles.Position = 0
+        SelectorsUpdate()
+    End Sub
+
+    Private Sub Anterior_Click(sender As Object, e As EventArgs) Handles Anterior.Click
+        bmb_articles.Position -= 1
+        SelectorsUpdate()
+    End Sub
+
+    Private Sub Seguent_Click(sender As Object, e As EventArgs) Handles Seguent.Click
+        bmb_articles.Position += 1
+        SelectorsUpdate()
+    End Sub
+
+    Private Sub Ultim_Click(sender As Object, e As EventArgs) Handles Ultim.Click
+        bmb_articles.Position = bmb_articles.Count - 1
+        SelectorsUpdate()
+    End Sub
+
+    Private Sub ToggleEditMode()
+        ModificarEstoc.Visible = Not ModificarEstoc.Visible
+        Cancelar.Visible = Not Cancelar.Visible
+        Acceptar.Visible = Not Acceptar.Visible
+
+        Crear.Visible = Not Crear.Visible
+        Modificar.Visible = Not Modificar.Visible
+        Eliminar.Visible = Not Eliminar.Visible
+
+        Primer.Visible = Not Primer.Visible
+        Anterior.Visible = Not Anterior.Visible
+        Ultim.Visible = Not Ultim.Visible
+        Seguent.Visible = Not Seguent.Visible
+
+        DescripcioArticle.ReadOnly = Not DescripcioArticle.ReadOnly
+        Preu.ReadOnly = Not Preu.ReadOnly
+        CodiFamilia.ReadOnly = Not CodiFamilia.ReadOnly
+        Comentaris.ReadOnly = Not Comentaris.ReadOnly
+    End Sub
+
+    Private Sub Crear_Click(sender As Object, e As EventArgs) Handles Crear.Click
+        bmb_articles.AddNew()
+        CodiArticle.Text = GetProxNumArticle()
+        ToggleEditMode()
+    End Sub
+
+    Private Sub Modificar_Click(sender As Object, e As EventArgs) Handles Modificar.Click
+        ToggleEditMode()
+    End Sub
+
+    Private Sub Cancelar_Click(sender As Object, e As EventArgs) Handles Cancelar.Click
+        bmb_articles.CancelCurrentEdit()
+        ToggleEditMode()
+        SelectorsUpdate()
+    End Sub
+
+    Private Sub Preu_TextChanged(sender As Object, e As EventArgs) Handles Preu.TextChanged
+        If Preu.Text.Length > 0 Then
+            Try
+                PreuAmbIVA.Text = Decimal.Parse(Preu.Text) * 1.21
+            Catch ex As Exception
+                MsgBox("El preu ha de ser un número valid")
+            End Try
+        End If
+    End Sub
+
+    Private Sub Acceptar_Click(sender As Object, e As EventArgs) Handles Acceptar.Click
+        bmb_articles.EndCurrentEdit()
+        If Not CanSaveCurrentEdit() Then
+            Return
+        End If
+        IncrementProxNumArticle()
+
+        If Not MakePersistencia() Then
+            Return
+        End If
+        ToggleEditMode()
+        SelectorsUpdate()
+    End Sub
+
+    Private Function CanSaveCurrentEdit()
+        If DescripcioArticle.Text.Length = 0 Then
+            MsgBox("El camp de la descripció no pot estar vuit")
+            Return False
+        End If
+        If CodiFamilia.Text.Length > 0 Then
+            If Not isCodiFamiliaValid(CodiFamilia.Text) Then
+                MsgBox("El codi de la familia no es valid")
+                Return False
+            End If
+        End If
+        Return True
+    End Function
+
+    Private Function isCodiFamiliaValid(codi As String)
+        Dim c As String
+        For index As Integer = 0 To ds_families.Tables("Families").Rows.Count - 1
+            c = ds_families.Tables("Families").Rows(index)("CODIFAMILIA")
+            If codi = c Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Private Sub ModificarEstoc_Click(sender As Object, e As EventArgs) Handles ModificarEstoc.Click
+        Dim estoc As String = InputBox("Introdueix un integer valid per l'estoc actual", "Estoc", EstocActual.Text)
+
+        If estoc.Length = 0 Then
+            Return
+        End If
+        Try
+            Integer.Parse(estoc)
+            EstocActual.Text = estoc
+        Catch ex As Exception
+            MsgBox("L'estoc ha de ser un integr valid!")
+        End Try
+    End Sub
+
+    Private Sub Eliminar_Click(sender As Object, e As EventArgs) Handles Eliminar.Click
+        If MessageBox.Show("Vols eliminar l'article?", "Alerta!!!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            bmb_articles.RemoveAt(bmb_articles.Position)
+
+            MakePersistencia()
+            SelectorsUpdate()
+        End If
+    End Sub
+
+    Private Function MakePersistencia()
+        Try
+            sda_articles.Update(ds_articles.Tables("Articles").GetChanges())
+            ds_articles.Tables("Articles").AcceptChanges()
+            Return True
+        Catch ex As Exception
+            MsgBox("Error: " + ex.Message)
+        End Try
+        Return False
+    End Function
+
+End Class
